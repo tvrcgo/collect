4a63737876c2151041afe4e56f67485b98c57e1b
tvrcgo <tvrcgo@gmail.com>
2015-12-07 20:08:29 +0800

    objectmode version.

--- index.js
+++ index.js
@@ -9,13 +9,30 @@ var proc = require('child_process');
 /**
  * Collect
  */
-function Collect() {
+function Collect(transform, flush) {
     if( !(this instanceof Collect)) {
-        return new Collect;
+        return new Collect(transform, flush);
     }
-    this._middleware = [];
     this._data = [];
-    Transform.call(this);
+    // inherits _transform
+    this._transform = transform || function(chunk, enc, done) {
+        if (chunk instanceof Buffer) {
+            this._data.push(chunk);
+        }
+        else {
+            this.push(chunk);
+        }
+        done();
+    };
+    // inherits _flush
+    this._flush = flush || function(done){
+        if (this._data.length) {
+            this.push({ content:this._data.toString() });
+        }
+        done();
+    };
+
+    Transform.call(this, { objectMode:true });
 }
 
 /**
@@ -25,90 +42,28 @@ function Collect() {
  */
 Object.setPrototypeOf(Collect.prototype, Transform.prototype);
 
-Collect.prototype._transform = function(chunk, encoding, done){
-    this._data.push(chunk);
-    done();
-}
-
-Collect.prototype._flush = function(done){
-    // parse body
-    var body = this._data.toString();
-    // process middlewares
-    var next = function(idx){
-        idx = idx || 0;
-        return function(data){
-            while(this._middleware[idx]) {
-                if (data instanceof Error) {
-                    if (this._middleware[idx].type!=="catch") {
-                        idx++;
-                        continue;
-                    }
-                }
-                else {
-                    if (this._middleware[idx].type!=='mw') {
-                        idx++;
-                        continue;
-                    }
-                }
-                break;
-            }
-            if (idx < this._middleware.length) {
-                try {
-                    this._middleware[idx].fn.call(this, data, next(++idx))
-                }
-                catch(err) {
-                    next(idx).call(this, err);
-                }
-            }
-            else {
-                this.push(data);
-            }
-        }.bind(this);
-    }.bind(this);
-    // start process.
-    next.call(this).call(this, body);
-
-    done();
-}
-
 /**
- * 中间件
- * @param  {function} mw 中间件函数
+ * 处理数据
+ * @param  {function} 处理函数
  * @return {stream}    Collect实例
  */
 Collect.prototype.use = function(mw){
-    if (typeof mw == 'function') {
-        this._middleware.push({ type:'mw', fn:mw });
+    if (mw instanceof Collect) {
+        return this.pipe(mw);
     }
-    if (mw instanceof this.constructor) {
-        this._middleware = this._middleware.concat(mw._middleware);
-    }
-    return this;
-}
 
-/**
- * 异常处理
- * @param  {function} mw
- * @return {object}  当前Collect实例
- */
-Collect.prototype.catch = function(mw){
-    if (typeof mw == 'function') {
-        this._middleware.push({ type:'catch', fn:mw });
+    if (typeof mw === 'function') {
+        return this.pipe(Collect(function(obj, enc, callback){
+            try {
+                mw.call(this, obj, function(tarObj){
+                    callback.call(this, null, tarObj);
+                });
+            }
+            catch(err) {
+                throw err;
+            }
+        }))
     }
-    return this;
-}
-
-/**
- * 处理结果保存到文件
- * @param  {String} filename
- */
-Collect.prototype.dest = function(filename, opts) {
-    opts = opts || {};
-    var dest = fs.createWriteStream(filename, {
-        flags: opts.flags || 'a',
-        encoding: opts.encoding || 'utf-8'
-    });
-    this.pipe(dest);
 }
 
 /**
@@ -120,24 +75,21 @@ Collect.prototype.dest = function(filename, opts) {
 Collect.src = function(url, opts){
     // Options.
     opts = opts || {};
-    // Collect instance
-    var co = Collect();
     // Fetch page content with ajax.
     if (opts.javascript) {
         var delay = opts.delay || 3000;
         var timeout = opts.timeout || 10*1000;
         var child = proc.spawn(phantomjs.path, [ __dirname + "/lib/asrc.js", url, delay, timeout ]);
         // filter phantom_echo content
-        co.use(function(data, next){
+        return child.stdout.pipe(Collect().use(function(data, next){
             if (data) {
-                data = data.split('PHANTOM_ECHO')[1];
+                data.content = data.content.split('PHANTOM_ECHO')[1];
                 next(data);
             }
             else {
-                throw new Error('src no data.');
+                throw new Error('asrc no data.');
             }
-        })
-        return child.stdout.pipe(co);
+        }));
     }
     // Default user-agent
     var UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36';
@@ -158,7 +110,20 @@ Collect.src = function(url, opts){
         j.setCookie(cookie, url);
         params.jar = j;
     }
-    return request(params).pipe(co);
+    return request(params).pipe(Collect());
+}
+
+/**
+ * 处理结果保存到文件
+ * @param  {String} filename
+ */
+Collect.prototype.dest = function(filename, opts) {
+    opts = opts || {};
+    var dest = fs.createWriteStream(filename, {
+        flags: opts.flags || 'a',
+        encoding: opts.encoding || 'utf-8'
+    });
+    this.pipe(dest);
 }
 
 Collect.query = require('./lib/query');
